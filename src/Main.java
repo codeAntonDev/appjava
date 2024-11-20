96a86caf954ae2d98baa7678e9418a4ff94eb784
Antonio Ramirez <hanscr.dsi.developer@gmail.com>
2024-11-20 14:23:08 -0500

    Agregando Repositorio del Curso

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        //String nombre = "Antonio";
+        //System.out.println(nombre);
+        //System.out.println("Hola Mundo");
+
+        /*
+         * ESTE
+         * ES
+         * UN COMENTARIO
+         * DE MULTIPLES
+         * LINEAS
+         * */
+
+        //int edad = 30;
+        //int numeroTelefono = 978978878;
+        //System.out.println(edad);
+        //System.out.println(numeroTelefono);
+
+        double salario = 1050.98;
+        System.out.println(salario);
+
+
+        boolean casado = true;
+        System.out.println(casado);
+
+
+        char palabraClave = 'A';
+        System.out.println(palabraClave);
+    }
+}
